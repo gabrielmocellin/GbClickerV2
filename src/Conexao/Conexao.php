694b33ff0c46ac10fd1c121e8d7004d12c861613
Gbex <gabriel.mocellin204@gmail.com>
2023-12-12 09:47:56 -0300

    Teste Mudança

--- src/Conexao/Conexao.php
+++ src/Conexao/Conexao.php
@@ -7,8 +7,8 @@ class Conexao
     public static function criarConexao()
     {
         $dsn = "mysql:host=localhost;dbname=gbclicker_db_mvc";
-        $user = "x";
-        $pass = "x";
+        $user = "gb";
+        $pass = "mysql@204";
         return new \PDO($dsn, $user, $pass);
     }
 }
